6b158477f48c65c51c5f76547f00aa5cc46b07d3
Trollking <kythorn@kakao.com>
2020-12-22 10:23:00 +0900

    .

--- sshcon.py
+++ sshcon.py
@@ -1,58 +1,201 @@
-import paramiko
+import paramiko as pm
 import pandas as pd
 import multiprocessing as mp
+import re
+import os
 from itaflogger import *
 from dbcon import *
 
 
 class SSHCtrl:
-	__cinf=pd.DataFrame()
-	__lgr=None
-	clients = []
-
+	__cinf = None
+	__lgr = None
+	
 	def __init__(self):
 		self.__lgr = Logger().getlogger("SSHCtrl")
 		dbc = DBCtrl()
 		dbc.connect()
 		columns = dbc.getcolumns('itaf', 'server_info')
 		columns.remove('ostype')
-		coninfo = dbc.select('itaf', 'server_info', "ostype != 'Windows'", cols=columns)
-		for col in columns:
-			self.__cinf[col]=[]
-		for row in coninfo:
-			for i,col in enumerate(columns):
-				self.__cinf[col].append(row[i])
-		self.__cinf['client']=[]
+		coninfo = dbc.select('itaf', 'server_info', "ostype != 'Windows'", \
+			cols=columns)
 		dbc.close()
+		self.__cinf = pd.DataFrame()
+		for col in columns:
+			self.__cinf[col] = []
+		for i, row in enumerate(coninfo):
+			temp = {}
+			for j, col in enumerate(columns):
+				temp[col] = row[j]
+			self.__cinf.loc[i] = temp
+		self.__cinf['client'] = None
+		self.__cinf = self.__cinf.astype({'port':int})
+		
 
 	def connect(self, hostip, hostport, userid, userpasswd):
 		client = pm.SSHClient()
 		client.set_missing_host_key_policy(pm.AutoAddPolicy())
 		try:
-			client.connect(hostip, port=hostport, username=userid, password=userpasswd)
+			client.connect(hostip, port=hostport, username=userid, password=userpasswd, timeout=10)
 		except Exception as e:
 			self.__lgr.error(e)
 			return None
 		return client
 
-	def connect_all(self) :
-		for r in self.__cinf.iloc:
-			if r['client'] != None or pd.isna(r['client']) == False:
-				continue
-			else:
-				r['client'] = connect(r['ip'], int(r['port']), r['userid'], r['passwd'])
+	def connectall(self, cno=None) :
+		'''
+		cno : [list] Client number ex)[0,2,6], None is All of clients
+		'''
+		ci = self.__cinf
+		if cno == None:
+			cno = range(len(self.__cinf.index))
 
-	def show_clients(self):
+		for rc in cno:
+			if ci['client'][rc] != None:
+				continue
+			else:	
+				ci['client'][rc] = self.connect(str(ci['ip'][rc]), int(ci['port'][rc]), str(ci['userid'][rc]), str(ci['passwd'][rc]))
+		
+
+	def showclients(self):
 		print(self.__cinf)
+		return self.__cinf
 
 	def runcmd(self, client, cmd):
+		'''
+		Run one or more Commands. There in no relation between Commands.
+		:client obj(SSHClient): paramko SSH Client object 
+		:cmd str or list : Command to run on the client
+		'''
+		if type(cmd) == str:
+			try:
+				stdin, stdout, stderr = client.exec_command(cmd)
+			except Exception as e:
+				self.__lgr.error(e)
+				return -1
+			return stdout,stderr
+		elif type(cmd) == list:
+			dict_ret = {'cmd':cmd, 'stdout':[], 'stderr':[]}
+			for line in enumerate(cmd):
+				try:
+					stdin, stdout, stderr = client.exec_command(line)
+				except Exception as e:
+					self.__lgr.error(e)
+					dict_ret['stdout'].append(None)
+					dict_ret['stderr'].append(None)
+					continue
+				dict_ret['stdout'].append(stdout.readlines())
+				dict_ret['stderr'].append(stderr.readlines())
+			return dict_ret
+		else:
+			self.__lgr.error('wrong type cmd parameter')
+			return -1
 
+	def runonshell(self, client, cmdlines):
+		'''
+		:param obj(SSHClient) client
+		:param list cmdlines
+		Run one or more Commands on interactive shell
+		'''
+		sh = client.invoke_shell()
+		dict_ret = {'cmd':cmdlines, 'recv':[]}
+		for cmd in cmdlines:
+			ret = sh.send(cmd)
+			buf = ''
+			while sh.recv_ready():
+				buf += sh.recv(4096)
+			dict_ret['recv'].append(re.split('\n|\r', buf.decode('ascii')))
+		sh.close()
+		return dict_ret
+
+	def isrmtfile(self, client, path):
+		cmd = "if test -f %s;then echo \"file\";"%path
+		cmd += "elif test -d %s;then echo \"directory\";fi"%path
+		dict_ret = self.runcmd(client, cmd)
+		if dict_ret['stdout'][0] == 'file':
+			return 1
+		elif dict_ret['stdout'][0] == 'directory':
+			return 2
+		else:
+			return -1
+
+	def getfileslist(self, path):
+		'''
+		searching all of files in srcpath and return list of files
+		'''
+		flist = []
+		for dpath, dnames, fnames in os.walk(path):
+			for fn in fnames:
+				flist.append(dpath + os.sep + fn)
+		return flist
+
+	def getlocalpath(self, path):
+		flist = []
+		dname = os.path.dirname(path)
+		bname = os.path.basename(path)
+		# Check whether the path is a directory
+		if os.path.isdir(path):
+			flist = self.getfileslist(path)
+			return flist
+		
+		# whether path is a file
+		if os.path.isfile(path):
+			flist.append(path)
+			return flist
+
+		# check the path includes wild cards and get files as the pattern
+		if re.search("\*|\?", bname) != None:
+			if re.fullmatch('\*+', bname):
+				patt = re.compile('\S*')
+			elif re.fullmatch('\?+', bname) != None:
+				patt = re.compile(bname.replace('?','.'))
+			else:
+				patt = '^' + bname.replace('.','\.').replace('?','.').replace('*','\S*') + '$'
+				patt = re.compile(patt)
+			
+			try:
+				dlist = os.listdir(dname)
+			except:
+				__lgr.error("%s directory does Not exist")
+				return flist
+
+			for fn in dlist:
+				if re.fullmatch(patt, fn) != None:
+					tmppath = dname + os.sep + fn
+					if os.path.isdir(tmppath):
+						flist += self.getfileslist(tmppath)
+					elif os.path.isfile(tmppath):
+						flist.append(tmppath)
+					else:
+						pass
+			return flist
+		return flist
+
+	def put(self, client, srcpath, dstpath):
+		sftp = pm.SFTPClient.from_transport(client.get_transport())
+		srcbase = os.path.basename(srcpath)
+		srcfiles = self.getlocalpath(srcpath)
+
+		# Check source path exists
+		if srcfiles == []:
+			__lgr.error('param srcpath Not exists')
+			return -1
+		
+		# Check dstpath exists
+		isrmt = self.isrmtfile(client, dstpath)
+		if self.isrmtfile(client, dstpath) != 2:
+			sftp.mkdir(dstpath)
+
+		# Check destination path on target server exist
+		# If directory does not exist, create directory
+		for path in srcfiles:
+			sftp.put(dstpath + os.sep + path.replace(srcbase,''))
+		sftp.close()
+
+	def get(self, client, dstpath):
 		pass
 
-	def putfile(self, client, srcpath, dstip, dstpath):
-		pass
-
-	def getfile(self, client, dstip, dstpath):
+	def runcmd_mp(self, cmd, cno=None, client=None):
 		pass
 
 	def close(self, idx):
@@ -67,5 +210,5 @@ class SSHCtrl:
 		else:
 			return 0
 
-
-
+if __name__ =='__main__':
+	pass
