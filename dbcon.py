0c2a78619c7cb2e27854efb8417e5fe1d158013f
Trollking <kythorn@kakao.com>
2021-01-13 15:57:28 +0900

    .

--- dbcon.py
+++ dbcon.py
@@ -7,40 +7,81 @@ CONF_PATH = 'conf/dbinfo.conf'
 
 class DBCtrl():
 	"""
-	conf/dbinfo.conf json형태의 설정파일을 바탕으로 
-	Mysql에 접속하여 DML을 수행하고 원하는 형태로 데이터를 리턴함
+	Mysql 접속/쿼리 수행하기 위한 클래스
+
+	Attribute:
+		__coninfo(Dict) : mysql 접속을 위한 접속 정보
+		__lgr(Obj:logger) : 인스턴스 상에서 발생하는 이벤트들에 대한 로거 object
+		db (Obj:DBconnector) : DB Connecter Object
+		cur (Ojb:DBCursor) : DB Cursor for querying
 	"""
-	
 	__coninfo = {}
 	__lgr = None
 	db = None
 	cur = None
 	
 	def __init__(self):
+		# json 형태의 dbinfo.conf 설정파일을 읽고 접속정보를 읽어옴
+
 		self.__lgr = Logger().getlogger("DBController")
 		try :
 			jf = open(CONF_PATH)
 			self.__coninfo = json.load(jf)
 			jf.close()
 		except Exception as e:
-			self.__lgr.error('%s %s'%(CONF_PATH,e))
+			self.__lgr.error(e)
 			raise e
 
-	def connect(self):
-		cinf = self.__coninfo
-		try:
-			self.db = mysql.connect(host=cinf['host'], port=int(cinf['port']), user=cinf['userid'], passwd=cinf['passwd'])
-		except Exception as e:
-			self.__lgr.error('%s %s'%(CONF_PATH,e))
-			raise e
+	#TODO (@jycho): Get params of DB coninfo and connect
+	def connect(self, host=None, port=None, user=None, passwd=None, db=None, charset=None):
+		"""
+		Connect to DB with __coninfo or parameters
+
+		Example:
+			connect("127.0.0.1","3306","root","toor",)
+
+		Args:
+			host(str) : DB host (Necessary)
+			port(int) : DB port (Default : 3306)
+			user(str) : DB userid (Necessary)
+			passwd(str) : User's Password
+			db(str) : DB name
+			charset(str) : DB's Charactor set  EX) "utf8". "euckr"
+
+		Returns:
+			db(obj) : DB Connector
+			cur(obj) : DB Cursor
+		"""
+		if host != None and user != None:
+			try:
+				self.db = mysql.connect(host=host, port=int(port), user=user, passwd=passwd, db=db, charset=charset)
+			except Exception as e:
+				self.__lgr.error(e)
+				raise e
+		else:
+			cif = self.__coninfo
+			try:
+				self.db = mysql.connect(host=cif['host'], port=int(cif['port']), user=cif['userid'],\
+passwd=cif['passwd'], db=cif['dbname'], charset=cif['charset'])
+			except Exception as e:
+				self.__lgr.error(e)
+				raise e
 		self.cur = self.db.cursor()
+		return self.db, self.cur
 
 	def isdbtbl(self, dbname='', tblname=''):
 		"""
-		Return Values 
-		0 : Both DB and table exist
-		-1 : DB does not exist
-		-2 : Table does not exist
+		Check DB and Table exists
+		
+		Args:
+			dbname : DB name to check
+			tblname : Table name to check
+
+		Returns :
+			Result of table existence(int)
+			0 : Both DB and table exist
+			-1 : DB does not exist
+			-2 : Table does not exist
 		"""
 		chkdbquery = "show databases like \'%s\'"%str(dbname)
 		chktblquery = "SELECT COUNT(*) FROM Information_schema.tables "
@@ -63,6 +104,14 @@ class DBCtrl():
 				return 0
 
 	def getcolumns(self, dbname='', tblname=''):
+		"""
+		Get specific column's label
+		Args:
+			dbname(str) : DB name
+			tblname(str) : Table name
+		Returns :
+			list of Column's label
+		"""
 		if self.isdbtbl(dbname, tblname) != 0 :
 			return -1
 		query='select * from %s.%s limit 1'%(dbname,tblname)
@@ -74,6 +123,17 @@ class DBCtrl():
 		return cols
 
 	def insert(self, dbname='', tblname='', value=[]):
+		"""
+		Run Insert query
+		Args:
+			dbname(str) : DB name
+			tblname(str) : Table name
+			value(list) : Data to insert
+		Returns:
+			Integer Result of insert query
+			0 < : Error
+			0 = : Success
+		"""
 		if self.isdbtbl(dbname, tblname) != 0 or value==[]:
 			self.__lgr.error('Could not Insert DATA')
 			return -1
@@ -90,7 +150,23 @@ class DBCtrl():
 		self.db.commit()
 
 	def select(self, dbname='', tblname='', case='', cols=[], without_header=True):
+		"""
+		Run select query
+		Example:
+
+		Args:
+			dbname(str) : DB name
+			tblname(str) : Table name
+			case(str) : Case without "where"
+			cols(list) : columns to return result
+			without_header(bool) : exclude header(col name)
+		Returns:
+			0 < : Error
+			[[]]] list : Result of select query
+			
+		"""
 		if self.isdbtbl(dbname, tblname) != 0:
+			self.__lgr.debug("db or table name not exist")
 			return -1
 
 		if len(cols) == 0 :
--- sshcon.py
+++ sshcon.py
@@ -1,6 +1,7 @@
 import paramiko as pm
 import pandas as pd
 import multiprocessing as mp
+import time
 import re
 import os
 from itaflogger import *
@@ -8,7 +9,15 @@ from dbcon import *
 
 
 class SSHCtrl:
-	__cinf = None
+	"""
+	SSH프로토콜을 사용하여 대상서버에서 작업하는 클래스
+	대상서버 접속정보는 DB에서 가져온다.
+
+	Attr:
+	cinf(DataFrame) : 대상서버 접속정보 및 ssh client 객체
+	__lgr (Logger) : 로깅을 위한 인스턴스
+	"""
+	cinf = None
 	__lgr = None
 	
 	def __init__(self):
@@ -20,25 +29,26 @@ class SSHCtrl:
 		coninfo = dbc.select('itaf', 'server_info', "ostype != 'Windows'", \
 			cols=columns)
 		dbc.close()
-		self.__cinf = pd.DataFrame()
+		self.cinf = pd.DataFrame()
 		for col in columns:
-			self.__cinf[col] = []
+			self.cinf[col] = []
 		for i, row in enumerate(coninfo):
 			temp = {}
 			for j, col in enumerate(columns):
 				temp[col] = row[j]
-			self.__cinf.loc[i] = temp
-		self.__cinf['client'] = None
-		self.__cinf = self.__cinf.astype({'port':int})
+			self.cinf.loc[i] = temp
+		self.cinf['client'] = None
+		self.cinf = self.cinf.astype({'port':int})
 		
 
-	def connect(self, hostip, hostport, userid, userpasswd):
+	def connect(self, host, port, user, passwd):
 		client = pm.SSHClient()
 		client.set_missing_host_key_policy(pm.AutoAddPolicy())
 		try:
-			client.connect(hostip, port=hostport, username=userid, password=userpasswd, timeout=10)
+			client.connect(host, port=port, username=user, password=passwd, timeout=5)
 		except Exception as e:
 			self.__lgr.error(e)
+			print("Error : %s"%e)
 			return None
 		return client
 
@@ -46,36 +56,48 @@ class SSHCtrl:
 		'''
 		cno : [list] Client number ex)[0,2,6], None is All of clients
 		'''
-		ci = self.__cinf
+		ci = self.cinf
 		if cno == None:
-			cno = range(len(self.__cinf.index))
+			cno = range(len(self.cinf.index))
 
 		for rc in cno:
+			print(rc)
 			if ci['client'][rc] != None:
 				continue
-			else:	
+			else:
 				ci['client'][rc] = self.connect(str(ci['ip'][rc]), int(ci['port'][rc]), str(ci['userid'][rc]), str(ci['passwd'][rc]))
+				print(ci['client'][rc])
 		
 
 	def showclients(self):
-		print(self.__cinf)
-		return self.__cinf
+		print(self.cinf)
+		return self.cinf
 
 	def runcmd(self, client, cmd):
 		'''
 		Run one or more Commands. There in no relation between Commands.
-		:client obj(SSHClient): paramko SSH Client object 
-		:cmd str or list : Command to run on the client
+		:param client : paramko SSH Client object 
+		:param cmd [str or list] : Command to run on the client
+		:return: dictionary of stdin, stdout, stderr
 		'''
+		dict_ret = {'cmd':cmd, 'stdout':[], 'stderr':[]}
+		if type(client) == pm.client.SSHClient:
+			pass
+		elif int(client) < len(self.cinf['client']) > 0:
+			client = self.cinf['client'][int(client)]
+		else :
+			self.__lgr.error('there is no %s client'%client)
+			return -1
+
 		if type(cmd) == str:
 			try:
 				stdin, stdout, stderr = client.exec_command(cmd)
 			except Exception as e:
 				self.__lgr.error(e)
 				return -1
-			return stdout,stderr
+			dict_ret['stdout'].append(stdout.readlines())
+			dict_ret['stderr'].append(stderr.readlines())
 		elif type(cmd) == list:
-			dict_ret = {'cmd':cmd, 'stdout':[], 'stderr':[]}
 			for line in enumerate(cmd):
 				try:
 					stdin, stdout, stderr = client.exec_command(line)
@@ -86,25 +108,41 @@ class SSHCtrl:
 					continue
 				dict_ret['stdout'].append(stdout.readlines())
 				dict_ret['stderr'].append(stderr.readlines())
-			return dict_ret
 		else:
 			self.__lgr.error('wrong type cmd parameter')
 			return -1
+		return dict_ret
 
 	def runonshell(self, client, cmdlines):
 		'''
+		Run one or more Commands on interactive shell
 		:param obj(SSHClient) client
 		:param list cmdlines
-		Run one or more Commands on interactive shell
 		'''
 		sh = client.invoke_shell()
 		dict_ret = {'cmd':cmdlines, 'recv':[]}
 		for cmd in cmdlines:
 			ret = sh.send(cmd)
 			buf = ''
-			while sh.recv_ready():
-				buf += sh.recv(4096)
-			dict_ret['recv'].append(re.split('\n|\r', buf.decode('ascii')))
+			cnt = 0
+			while True:
+				if sh.recv_ready():
+					buf += sh.recv(4096).decode('ascii')
+				else :
+					if buf == '':
+						cnt += 1
+						time.sleep(0.1)
+						if cnt > 10:
+							cnt = -1
+							break
+						else :
+							continue
+					else:
+						break
+			if cnt == -1 :
+				dict_ret['recv'].append('')
+			else:
+				dict_ret['recv'].append(re.split('\n|\r', buf))
 		sh.close()
 		return dict_ret
 
@@ -118,18 +156,32 @@ class SSHCtrl:
 			return 2
 		else:
 			return -1
-
-	def getfileslist(self, path):
+	
+	def getfileslist(self, path, client=None):
 		'''
-		searching all of files in srcpath and return list of files
+		path에 해당되는 디렉토리내 모든 파일 및 디렉토리 목록을 리턴한다.
+		client를 입력받을 경우 원격지의 path에 대해서 수행한다.
+		:param path: 디렉토리 경로
+		:param client: ssh client object
 		'''
 		flist = []
-		for dpath, dnames, fnames in os.walk(path):
-			for fn in fnames:
-				flist.append(dpath + os.sep + fn)
+		if client != None :
+			cmd = "file `find %s`|grep -v directory"%path
+			ret = self.runcmd(client, cmd)
+			if len(ret['stdout'][0]) > 0 :
+				for line in ret['stdout'][0]:
+					flist.append(line.strip(':')[0])
+		else:
+			for dpath, dnames, fnames in os.walk(path):
+				for fn in fnames:
+					flist.append(dpath + os.sep + fn)
 		return flist
 
 	def getlocalpath(self, path):
+		'''
+		path가 존재하는지 파일인지 디렉토리인지 확인하고 디렉토리일경우 디렉토리 하위의 모든 파일들을 리턴한다.
+		path의 파일명에 와일드카드(*,?)가 존재할경우 다수의 파일목록을 리턴한다.
+		'''
 		flist = []
 		dname = os.path.dirname(path)
 		bname = os.path.basename(path)
@@ -172,6 +224,12 @@ class SSHCtrl:
 		return flist
 
 	def put(self, client, srcpath, dstpath):
+		'''
+		ssh 클라이언트를 통해 원본경로의 내용을 대상경로로 업로드한다.
+		:client: ssh client object
+		:srcpath: source path to upload
+		:dstpath: target path to upload
+		'''
 		sftp = pm.SFTPClient.from_transport(client.get_transport())
 		srcbase = os.path.basename(srcpath)
 		srcfiles = self.getlocalpath(srcpath)
@@ -181,34 +239,41 @@ class SSHCtrl:
 			__lgr.error('param srcpath Not exists')
 			return -1
 		
-		# Check dstpath exists
+		# Check destination path on target server exist
+		# If directory does not exist, create directory
 		isrmt = self.isrmtfile(client, dstpath)
 		if self.isrmtfile(client, dstpath) != 2:
 			sftp.mkdir(dstpath)
 
-		# Check destination path on target server exist
-		# If directory does not exist, create directory
 		for path in srcfiles:
 			sftp.put(dstpath + os.sep + path.replace(srcbase,''))
 		sftp.close()
 
 	def get(self, client, dstpath):
+		# TODO(jycho) : implement get function
+		# Create SFTPClient
+		sftp = pm.SFTPClient.from_transport(client.get_transport())
+		dstfiles = []
+		# Check Dstpath
+
+
+		# get directory or files
 		pass
 
 	def runcmd_mp(self, cmd, cno=None, client=None):
 		pass
 
 	def close(self, idx):
-		client = self.__cinf['client'][idx]
+		client = self.cinf['client'][idx]
 		if client != -1 or pd.isna(client) == False:
 			try :
 				client.close()
 			except Exception as e:
 				self.__lgr.error(e)
 				return -1
-			self.__cinf['client'][idx] == -1
+			self.cinf['client'][idx] == -1
 		else:
 			return 0
 
-if __name__ =='__main__':
+if __name__ == '__main__':
 	pass
