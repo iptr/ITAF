ea4931b9ec5ed5195a3fd4278087e08dfc877439
myysh17 <myysh17@gmail.com>
2021-04-07 16:13:28 +0900

    0407

---
+++ dbsafergui.py
@@ -0,0 +1,184 @@
+import winguicommon
+
+DEFAULTICONPATH = "icon/"
+#TODO : 여러 가지 방향성을 조금 더 고민해 보아야 할 필요가 있다.
+class Dbsafergui:
+    def __init__(self):
+        pass
+
+    def start(self,path):
+        '''
+        DBSAFER Manger 시작
+
+        @param
+            path - 매니저 실행 파일 경로
+
+        @return
+            True - 성공
+            False - 실패
+        '''
+        if winguicommon.runExe(path) == False:
+            return False
+        try:
+            # 디바이스 변경 허용 '예' 클릭
+            cursor = winguicommon.findLocationPicture(DEFAULTICONPATH+"yes.png")
+            winguicommon.clickMouse(cursor)
+        except Exception as e:
+            return False
+
+        return True
+
+    def login(self):
+        '''
+        Manger login
+
+        @return
+            True - 성공
+            False - 실패
+        '''
+        try:
+            # 이미지 찾기를 이용하여 ip 입력
+            cursor = winguicommon.findLocationPicture(DEFAULTICONPATH+"log_in_ip.png")
+            winguicommon.clickMouse(cursor)
+            winguicommon.inputMsg("10.77.161.167")
+
+            # login ID 입력
+            winguicommon.inputTab()
+            winguicommon.inputMsg("heyboy")
+
+            # login PassWord 입력
+            winguicommon.inputTab()
+            winguicommon.inputMsg("dbsafer00")
+
+            # Connect 클릭
+            cursor = winguicommon.findLocationPicture(DEFAULTICONPATH + "log_in_connect.png")
+            winguicommon.clickMouse(cursor)
+
+            # 비밀 번호 바꾸라는 경고, 알림 등을 끌 때 사용
+            # TODO : 좌표값을 얻지 않고도 무조건 해당 알림을 종료 할 수 있는 방법을 찾자
+            # TODO : new window 가 발생 했을 때 이벤트를 감지 할 수 있는거 찾아 보자
+                # 1. esc 를 2번 누른다.
+                    # 문제점: 해당 화면(알림)이 언제 뜰지 모른다.
+                # 2. 이미지를 찾아 간다.
+                    # 비밀 번호 바꾸라는 경고가 뜨는경우 안 뜨는 경우를 구분해야 한다.
+                # 3. 좌표값을 찾아 간다.
+                    # 매니저 위치, 크기가 일정하다는 가정이 필요
+            winguicommon.inputEsc()
+            winguicommon.inputEsc()
+
+        except Exception as e:
+            return False
+
+        return True
+
+    def viewControlPolicy(self):
+        '''
+        정책 제어 화면 출력
+
+        @return
+            True - 성공
+            False - 실패
+        '''
+        try:
+            # 정책 제어 아이콘 찾아 클릭
+            cursor = winguicommon.findLocationPicture(DEFAULTICONPATH+"control_policy.png")
+            winguicommon.clickMouse(cursor)
+        except Exception as e:
+            return False
+
+        return True
+
+    def viewControlObject(self):
+        '''
+        객체 제어 화면 출력
+
+        @return
+            True - 성공
+            False - 실패
+        '''
+        try:
+            # 객체 제어 아이콘 찾아 클릭
+            cursor = winguicommon.findLocationPicture(DEFAULTICONPATH + "control_object.png")
+            winguicommon.clickMouse(cursor)
+        except Exception as e:
+            return False
+
+        return True
+
+    def viewMonitoring(self):
+        '''
+        모니터링 화면 출력
+
+        @return
+            True - 성공
+            False - 실패
+        '''
+        try:
+            # 모니터링 아이콘 찾아 클릭
+            cursor = winguicommon.findLocationPicture(DEFAULTICONPATH + "monitor.png")
+            winguicommon.clickMouse(cursor)
+        except Exception as e:
+            return False
+
+        return True
+
+    def viewLog(self):
+        '''
+        로그 조회 화면 출력
+
+        @return
+            True - 성공
+            False - 실패
+        '''
+        try:
+            # 로그 조회 아이콘 찾아 클릭
+            cursor = winguicommon.findLocationPicture(DEFAULTICONPATH + "log.png")
+            winguicommon.clickMouse(cursor)
+        except Exception as e:
+            return False
+
+        return True
+
+    def viewMaintenance(self):
+        '''
+        유지 보수 화면 출력
+
+        @return
+            True - 성공
+            False - 실패
+        '''
+        try:
+            # 유지 보수 아이콘 찾아 클릭
+            cursor = winguicommon.findLocationPicture(DEFAULTICONPATH + "maintenance.png")
+            winguicommon.clickMouse(cursor)
+        except Exception as e:
+            return False
+
+        return True
+
+    def viewSetting(self):
+        '''
+        설정 화면 출력
+
+        @return
+            True - 성공
+            False - 실패
+        '''
+        try:
+            # 설정 아이콘 찾아 클릭
+            cursor = winguicommon.findLocationPicture(DEFAULTICONPATH + "setting.png")
+            winguicommon.clickMouse(cursor)
+        except Exception as e:
+            return False
+
+        return True
+
+
+if __name__ == '__main__':
+    a = Dbsafergui()
+    a.login()
+    # a.viewControlPolicy()
+    # a.viewControlObject()
+    # a.viewMonitoring()
+    # a.viewSetting()
+    # a.viewLog()
---
+++ winguicommon.py
@@ -0,0 +1,323 @@
+import subprocess
+import pyscreenshot
+import pyautogui
+
+def runWindowCommand(command=""):
+    '''
+    cmd 명령어 실행 결과 확인
+
+    @param
+        command - cmd 명령어
+
+    @return
+        명령 실행 결과
+    '''
+    result = ""
+    try:
+        # 커맨드 명령 실행
+        result = subprocess.call(command,shell=True)
+    except Exception as e:
+        #todo:logging
+        print(e)
+        return ""
+
+    return result
+def runCapture(start_x = 0, start_y = 0, x = 640, y=480,path=""):
+    '''
+    화면 캡쳐 실행 및 저장
+
+    @param
+        start_x - x 시작 좌표
+        start_y - y 시작 좌표
+        x - x 좌표
+        y - y 좌표
+
+    @return
+        True - 성공
+        False - 실패
+    '''
+    try:
+        # 각 좌표를 기준으로 캡쳐 실행
+        img = pyscreenshot.grab(bbox=(start_x,start_y,x,y))
+        # 캡쳐본 지정된 경로에 저장
+        img.save(path)
+    except Exception as e:
+        #todo:logging
+        print(e)
+        return False
+
+    return True
+
+def runExe(path=""):
+    '''
+    외부 실행 프로그램 실행
+
+    @param
+        path - 외부 실행 프로그램 경로
+
+    @return
+        True - 성공
+        False - 실패
+    '''
+    try:
+        # 지정된 경로에 있는 응용 프로그램 실행
+        subprocess.call(path)
+    except Exception as e:
+        #todo:logging
+        print(e)
+        return False
+
+    return True
+
+def getCursor():
+    '''
+    현재 커서 위치 반환
+
+    @return
+        현재 커서 위치
+    '''
+    pos = ()
+    try:
+        pos = pyautogui.position()
+    except Exception as e:
+        return pos
+
+    return pos
+
+def getMonitorSize():
+    '''
+    현재 모니터 해상도 확인
+
+    @return
+        해상도
+    '''
+    size = 0
+
+    try:
+        size = pyautogui.size()
+    except Exception as e:
+        return -1
+
+    return size
+
+def moveCursor(pos):
+    '''
+    절대적인 위치로 커서 이동
+
+    @param
+        pos - x,y 좌표
+
+    @return
+        True - 성공
+        False - 실패
+    '''
+    try:
+        pyautogui.FAILSAFE = False
+        pyautogui.moveTo(pos)
+    except Exception as e:
+        #todo:logging
+        return False
+
+    return True
+
+def clickMouse(pos=(),click=1,interval=1,button="left",double=0):
+    '''
+    마우스 클릭
+
+    @param
+        pos - x,y 좌표
+        click - 원하는 클릭 횟수
+        interval - 클릭 간격
+        button - 마우스 버튼
+        double - 더블 클릭 여부
+
+    @return
+        True - 성공
+        False - 실패
+    '''
+    try:
+        if len(pos) == 0:
+            pyautogui.click()
+        else:
+            pyautogui.FAILSAFE = False
+            if double == 0:
+                # 클릭
+                pyautogui.click(pos,clicks=click,interval=interval,button=button)
+            elif double == 1:
+                pyautogui.doubleClick(pos,button=button)
+            else:
+                raise Exception("클릭 실패!")
+    except Exception as e:
+        return False
+
+    return True
+
+def changeMouseRelativePositon(pos):
+    '''
+    마우스 포인터 상대적으로 이동
+
+    @param
+        pos - x,y 좌표
+
+    @return
+        True - 성공
+        False - 실패
+    '''
+    try:
+        pyautogui.moveRel(pos)
+    except Exception as e:
+        return False
+
+    return True
+
+def drag(pos,persistence=1,button="left",relative=0):
+    '''
+    원하는 영역 드래그
+
+    @param
+        pos - x,y 좌표
+        persistence - 지속 시간
+        button - 마우스 버튼
+        relative - 상대적 좌표값 사용 여부
+
+    @return
+        True - 성공
+        False - 실패
+    '''
+    try:
+        # 절대 좌표를 사용 하는 경우
+        if relative == 0:
+            pyautogui.dragTo(pos,duration=persistence,button=button)
+        # 상대 좌표를 사용 하는 경우
+        elif relative == 1:
+            pyautogui.dragRel(pos,duration=persistence,button=button)
+        else:
+            raise Exception("드래그 실패!")
+    except Exception as e:
+        return False
+
+    return True
+
+def inputMsg(message=""):
+    '''
+    응용 프로그램에 입력값 전달
+
+    @param
+        message - 입력값
+
+    @return
+        True - 성공
+        False - 실패
+    '''
+    try:
+        # 해당 문자를 입력
+        pyautogui.typewrite(message)
+    except Exception as e:
+        return False
+
+    return True
+
+def upDownOneKey(charactor):
+    '''
+    하나의 키만 입력
+
+    @param
+        charactor - 입력하고자하는 문자
+
+    @return
+        True - 성공
+        False - 실패
+    '''
+    if len(charactor) != 1:
+        return False
+
+    try:
+        # 해당 문자를 누름
+        pyautogui.keyDown(charactor)
+        # 키보드 자판을 누른 후 뗌
+        pyautogui.keyUp(charactor)
+    except Exception as e:
+        return False
+
+    return True
+
+def findLocationPicture(path):
+    '''
+    사진을 이용하여 해당 사진의 내용이 존재하는 좌표 값을 반환
+
+    @param
+        path - 사진 경로
+
+    @return
+        해당 좌표값 반환
+    '''
+    picture_center = ()
+    try:
+        # 사진의 내용이 존재하는 좌표값 확인
+        picture = pyautogui.locateOnScreen(path)
+        # 좌표 값의 가운데를 클릭
+        picture_center=pyautogui.center(picture)
+    except Exception as e:
+        print(e)
+        return picture_center
+
+    return picture_center
+
+def closeCurrentWindow():
+    '''
+    현재 창 닫기
+
+    @return
+        True - 성공
+        False - 실패
+    '''
+    try:
+        # alt + F4 종료 커맨드 실행
+        pyautogui.hotkey('alt','f4')
+    except Exception as e:
+        return False
+
+    return True
+
+def inputTab():
+    '''
+    키보드의 tab을 입력
+
+    @return
+        True - 성공
+        False - 실패
+    '''
+    try:
+        pyautogui.press('tab')
+    except Exception as e:
+        return False
+
+    return True
+
+def inputEsc():
+    '''
+    키보드의 ESC를 입력
+
+    @return
+        True - 성공
+        False - 실패
+    '''
+    try:
+        pyautogui.press('esc')
+    except Exception as e:
+        return False
+
+    return True
+
+if __name__ == '__main__':
+    #moveCursor((1200,100))
+    #clickMouse(0,0)
+    #a = findLocationPicture("스크린샷, 2021-04-07 13-40-25.png")
+    #clickMouse()
+    # inputTab()
+    # inputTab()
+    # inputTab()
+    # inputTab()
+    print(pyautogui.getInfo())
+    fw = pyautogui.getActiveWindow()
+    print(fw.width)
