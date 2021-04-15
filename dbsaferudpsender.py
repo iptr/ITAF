7b83c3cd5a716a9f19ce4570652f857bad16e2f8
myysh17 <myysh17@gmail.com>
2021-04-15 17:52:06 +0900

    [dbsaferudpsender.py] [shyoon] [RV:N/A] [BT:N/A] [이슈사항] dbsafer 정책 변경시 특정 포트로 UDP 패킷 송신
    [수정사항]
    port = [21114 ,50011 ,3140 ,3159 ,3161 ,3143 ,3158 ,3160, 3120,3126]
    UDP 패킷 0x00 0x01 송신

---
+++ dbsaferudpsender.py
@@ -0,0 +1,19 @@
+import packetcontroler as pt
+
+def dbsaferUdpPacket():
+    packet = pt.Packet()
+    host = "127.0.0.1"
+    port = [21114 ,50011 ,3140 ,3159 ,3161 ,3143 ,3158 ,3160, 3120,3126]
+    data = bytes.fromhex('00')
+    for i in range(len(port)):
+        packet.connectUdp(host,port[i])
+        packet.sendUdpPacket(data)
+        packet.closeUdpPacket()
+    data = bytes.fromhex('01')
+    for i in range(len(port)):
+        packet.connectUdp(host, port[i])
+        packet.sendUdpPacket(data)
+        packet.closeUdpPacket()
+
+if __name__ == '__main__':
+    dbsaferUdpPacket()
