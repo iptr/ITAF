d867b51880ec22087736ba30d2b7afce5d3a0d40
myysh17 <myysh17@gmail.com>
2021-05-12 17:51:22 +0900

    [packetutil.py] [shyoon] [RV:N/A] [BT:N/A]
    [추가사항]
    RDP Server용 소켓, Client용 소켓 분리하여 관리

--- packetutil.py
+++ packetutil.py
@@ -82,7 +82,7 @@ class PacketReader:
 class VirtualConnector:
     lock = Lock()
 
-    def __init__(self, service_port, dbsafer_ip, dbsafer_port):
+    def __init__(self,  service_port, dbsafer_ip, dbsafer_port):
         '''
 
         @param
@@ -93,54 +93,94 @@ class VirtualConnector:
         self.dbsafer_ip = dbsafer_ip
         self.dbsafer_port = dbsafer_port
         self.service_port = service_port
-        self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.serversocket.bind(('', service_port))
-        self.serversocket.listen(100)
 
     async def dbModeConnect(self):
         self.lock.acquire()
         try:
             service_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             service_sock.connect((self.dbsafer_ip, self.dbsafer_port))
-            (db_sock, address) = self.serversocket.accept()
-            time.sleep(10)
-            return (db_sock, service_sock)
+            return (service_sock)
         except Exception as e:
             print("Session Error")
             print(e)
         finally:
             self.lock.release()
 
-    async def rdpModeConnect(self):
+    def rdpModeConnect(self):
         self.lock.acquire()
         try:
             nat = termctrl.NATIDPKT()
             telnet_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            print(self.dbsafer_port)
             telnet_socket.connect((self.dbsafer_ip, self.dbsafer_port))
-            telnet_socket.send(nat.set(tgip="10.77.162.11",tgport=self.service_port,gwport=self.dbsafer_port,gwip="192.168.4.190",certid="shyoon",loip=telnet_socket.getsockname()[0],loport=telnet_socket.getsockname()[1]))
+            telnet_socket.send(nat.set(tgip="10.77.162.13",tgport=self.service_port,gwport=self.dbsafer_port,gwip="192.168.4.190",certid="shyoon",loip=telnet_socket.getsockname()[0],loport=telnet_socket.getsockname()[1]))
             # 필수정보 교체: 대상서비스번호, 대상IP, port, 로컬IP, 로컬port, dbsIP, dbsport,
             #               보안계정)))
-            wta_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            (terminal_socket, address) = self.serversocket.accept()
-            wta_socket.connect((self.dbsafer_ip, 3140))
-            w = wtapacket.WtaPacketMaker()
-            w.makePacket(client_ip="10.77.162.11", client_port=self.service_port, wta_proxy_port=3141,
-             				 service_port=self.dbsafer_port,
-             				 service_ip="192.168.4.190")
-            print(w.encryptPacket())
-            wta_socket.send(w.encryptPacket())
-            print(terminal_socket)
-            print(telnet_socket)
-            print(wta_socket)
-            return (terminal_socket, telnet_socket, wta_socket)
+            #print(w.encryptPacket())
+            #test.send(w.encryptPacket())
+            # print(terminal_socket)
+            # print(telnet_socket)
+            # print(wta_socket)
+            return (telnet_socket)
         except Exception as e:
             print("Session Error")
             print(e)
         finally:
             self.lock.release()
 
-    def destroy(self):
-        self.serversocket.close()
+        # async def rdpModeConnect(self):
+        #     self.lock.acquire()
+        #     try:
+        #         nat = termctrl.NATIDPKT()
+        #         telnet_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #         print(self.dbsafer_port)
+        #         telnet_socket.connect((self.dbsafer_ip, self.dbsafer_port))
+        #         telnet_socket.send(nat.set(tgip="10.77.162.11", tgport=self.service_port, gwport=self.dbsafer_port,
+        #                                    gwip="192.168.4.190", certid="shyoon", loip=telnet_socket.getsockname()[0],
+        #                                    loport=telnet_socket.getsockname()[1]))
+        #         # 필수정보 교체: 대상서비스번호, 대상IP, port, 로컬IP, 로컬port, dbsIP, dbsport,
+        #         #               보안계정)))
+        #         wta_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #         (terminal_socket, address) = self.serversocket.accept()
+        #         wta_socket.connect((self.dbsafer_ip, 3141))
+        #         wta_info_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #         wta_info_socket.connect((self.dbsafer_ip, 3140))
+        #
+        #         w = wtapacket.WtaPacketMaker()
+        #         w.makePacket(client_ip="10.77.162.11", client_port=self.service_port, wta_proxy_port=3141,
+        #                      service_port=self.dbsafer_port,
+        #                      service_ip="192.168.4.190")
+        #         # print(w.encryptPacket())
+        #         # test.send(w.encryptPacket())
+        #         # print(terminal_socket)
+        #         # print(telnet_socket)
+        #         # print(wta_socket)
+        #
+        #         return (terminal_socket, telnet_socket, wta_socket, wta_info_socket)
+        #     except Exception as e:
+        #         print("Session Error")
+        #         print(e)
+        #     finally:
+        #         self.lock.release()
+class VirtualServer:
+    def __init__(self,service_port,dbsafer_ip,dbsafer_port):
+        self.dbsafer_ip = dbsafer_ip
+        self.dbsafer_port = dbsafer_port
+        self.service_port = service_port
+        self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.serversocket.bind(('', service_port))
+        self.serversocket.listen(100)
 
+    def rdpModeServer(self):
+        try:
+            wta_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            wta_socket.connect((self.dbsafer_ip, 3141))
+            wta_info_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            wta_info_socket.connect((self.dbsafer_ip, 3140))
+            print(wta_info_socket,wta_socket)
+            (terminal_socket, address) = self.serversocket.accept()
+
+            return (terminal_socket,wta_socket,wta_info_socket)
+        except Exception as e:
+            print("Session Error")
+            print(e)
