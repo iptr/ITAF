327c18b7e127b6282ba5d805551272e8803230cf
myysh17 <myysh17@gmail.com>
2021-05-27 16:52:06 +0900

    [dbmsserver.py] [shyoon] [RV:N/A] [BT:N/A]
    [추가사항]
    CONFIG 파일 적용

--- dbmsserver.py
+++ dbmsserver.py
@@ -13,252 +13,258 @@ import packetutil
 
 # 스택 사이즈 결정
 if sys.version_info >= (3, 5):
-	threading.stack_size(1024 * 1024 * 1024 * 2)
+    threading.stack_size(1024 * 1024 * 1024 * 2)
 
 # 기본 타임아웃 시간 설정
 #socket.setdefaulttimeout(100)
 
 class Worker(multiprocessing.Process):
 
-	def __init__(self, num, connector, packets,
-				 timeout, repeat, sleep, verbose,
-				 callback, callback_arg,thread_count,que):
-		multiprocessing.Process.__init__(self)
-		self.num = num
-		self.connector = connector
-		self.packets = packets
-		self.timeout = timeout
-		self.repeat = repeat
-		self.sleep = sleep
-		self.verbose = verbose
-		self.callback = callback
-		self.callback_arg = callback_arg
-		self.cancelFlag = False
-		self.progressCallback = None
-		self.progressCallbackArg = None
-		self.thread_count = thread_count
-		self.q = que
+    def __init__(self, num, dbms_sock_object, packets,
+                 conf, target_list, callback, callback_arg, q):
+        multiprocessing.Process.__init__(self)
+        self.num = num
+        self.dbms_sock_object = dbms_sock_object
+        self.packets = packets
+        self.conf = conf
+        self.target_list = target_list
+        self.callback = callback
+        self.callback_arg = callback_arg
+        self.cancelFlag = False
+        self.progressCallback = None
+        self.progressCallbackArg = None
+        self.q = q
 
-	def setProgressCallback(self, callback, callbackarg):
-		self.progressCallback = callback
-		self.progressCallbackArg = callbackarg
+    def setProgressCallback(self, callback, callbackarg):
+        self.progressCallback = callback
+        self.progressCallbackArg = callbackarg
 
-	def run(self):
-		'''
-		시작 하는 함수
+    def run(self):
+        '''
+        시작 하는 함수
 
-		'''
-		thread_list = []
+        '''
+        thread_list = []
 
-		# 반복 횟수가 0 일경우
-		if self.repeat == 0:
-				pass
-		# 반복 횟수가 지정되어 있을 경우
-		else:
-			for i in range(self.repeat):
-				# 쓰레드 생성
-				for j in range(self.thread_count):
-					thread = threading.Thread(target=self.test)
-					thread_list.append(thread)
-				for j in thread_list:
-					j.start()
-				for j in thread_list:
-					j.join()
+        # 반복 횟수가 0 일경우
+        thread_list = []
 
-				if (self.cancelFlag):
-					break
+        # 반복 횟수가 0 일경우
+        if self.conf['REPEAT_COUNT'] == 0:
+            pass
+        # 반복 횟수가 지정되어 있을 경우
+        else:
+            for i in range(int(self.conf['REPEAT_COUNT'])):
+                # 쓰레드 생성
+                for j in range(int(self.conf['THREAD_PER_PROC'])):
+                    thread = threading.Thread(target=self.test)
+                    thread_list.append(thread)
+                for j in thread_list:
+                    j.start()
+                for j in thread_list:
+                    j.join()
 
-		if self.cancelFlag:
-			print("Job canceled %d." % self.num)
-		else:
-			print("End %d." % self.num)
+                if (self.cancelFlag):
+                    break
 
-	def cancel(self):
-		self.cancelFlag = True
+        if self.cancelFlag:
+            print("Job canceled %d." % self.num)
+        else:
+            print("End %d." % self.num)
 
-	def test(self):
-		'''
-		패킷 테스트 하는 함수
-		'''
-		if self.cancelFlag:
-			self.callback(self.callback_arg)
-			return
+    def cancel(self):
+        self.cancelFlag = True
 
-		# try:
-		print("Connecting %d..." % self.num)
-		# 각 세션 소켓 반환
-		(dbms_sock) = self.connector.dbmsModeServer()
-		self.q.put(dbms_sock)
+    def test(self):
+        '''
+        패킷 테스트 하는 함수
+        '''
+        if self.cancelFlag:
+            self.callback(self.callback_arg)
+            return
 
-		print("Connected %d." % self.num)
+        # try:
+        print("Connecting %d..." % self.num)
+        # 각 세션 소켓 반환
+        (dbms_sock) = self.dbms_sock_object.dbmsModeServer()
+        self.q.put(dbms_sock)
 
+        print("Connected %d." % self.num)
 
-		pos = 0
-		packet = ''
-		# 패킷 길이 확인 및 전송
-		while pos < len(self.packets):
-			if pos < len(self.packets):
-				packet = self.packets[pos]
-			pos_end = pos + 1
+        while True:
+            pos = 0
+            packet = ''
+            # 패킷 길이 확인 및 전송
+            while pos < len(self.packets):
+                if pos < len(self.packets):
+                    packet = self.packets[pos]
+                pos_end = pos + 1
 
-			while pos_end < len(self.packets):
-				packet2 = self.packets[pos_end]
-				if packet.direction != packet2.direction:
-					break
-				pos_end += 1
+                while pos_end < len(self.packets):
+                    packet2 = self.packets[pos_end]
+                    if packet.direction != packet2.direction:
+                        break
+                    pos_end += 1
 
-			if dbms_sock:
-				pass
-			else:
-				break
+                if dbms_sock:
+                    pass
+                else:
+                    break
 
-			try:
-				# 패킷 전송
-				self.send_packet(pos, pos_end, dbms_sock)
-				time.sleep(0.3)
-			except socket.timeout:
-				print('T(%d/0)' % len(packet.packet), end=' ')
-				sys.stdout.flush()
-			if self.progressCallback:
-				self.progressCallback(self.progressCallbackArg, pos_end - pos)
-			pos = pos_end
-			print(pos,pos_end,len(self.packets))
-			if self.cancelFlag:
-				break
+                try:
+                    # 패킷 전송
+                    self.send_packet(pos, pos_end, dbms_sock)
+                    time.sleep(0.3)
+                except socket.timeout:
+                    print('T(%d/0)' % len(packet.packet), end=' ')
+                    sys.stdout.flush()
+                if self.progressCallback:
+                    self.progressCallback(self.progressCallbackArg, pos_end - pos)
+                pos = pos_end
+                print(pos,pos_end,len(self.packets))
+                if self.cancelFlag:
+                    break
 
-		if self.sleep != 0:
-			time.sleep(self.sleep)
+            if int(self.conf['SLEEP']) != 0:
+                time.sleep(int(self.conf['SLEEP']))
 
-		print("Session wait %d." % self.num)
+            print("Session wait %d." % self.num)
 
-		self.callback(self.callback_arg)
-
+            self.callback(self.callback_arg)
 
 
-	def send_packet(self, pos, pos_end, dbms_sock):
-		'''
-		select를 이용하여 패킷 송수신을 담당하는 함수
-		'''
-		try:
-			sendedPacket = 0
-			sended = 0
-			recvedPacket = 0
-			recved = 0
-			direction = self.packets[pos].direction
-			if direction:
-				inputs = [dbms_sock, ]
-				outputs = [ ]
-			else:
-				inputs = [ ]
-				outputs = [dbms_sock, ]
 
-			packetLen = pos_end - pos
+    def send_packet(self, pos, pos_end, dbms_sock):
+        '''
+        select를 이용하여 패킷 송수신을 담당하는 함수
+        '''
+        try:
+            sendedPacket = 0
+            sended = 0
+            recvedPacket = 0
+            recved = 0
+            direction = self.packets[pos].direction
+            if direction:
+                inputs = [dbms_sock, ]
+                outputs = [ ]
+            else:
+                inputs = [ ]
+                outputs = [dbms_sock, ]
 
-			(readable, writeable, exceptional) = select.select(inputs, outputs, inputs)
+            packetLen = pos_end - pos
 
-			if not (readable or writeable or exceptional):
-				# timeout
-				while recvedPacket < packetLen:
-					sys.stdout.flush()
-					recved = 0
-					recvedPacket += 1
-				return
+            (readable, writeable, exceptional) = select.select(inputs, outputs, inputs)
 
-			for s in readable:
-				sendSize = 0
-				if pos + recvedPacket < len(self.packets):
-					sendSize = len(self.packets[pos].packet)
-				r = s.recv(100000000)
+            if not (readable or writeable or exceptional):
+                # timeout
+                while recvedPacket < packetLen:
+                    sys.stdout.flush()
+                    recved = 0
+                    recvedPacket += 1
+                return
 
-				while r:
-					if recvedPacket >= packetLen:
-						print('X', end=' ')
-						sys.stdout.flush()
-						break
-					packet = self.packets[pos + recvedPacket].packet
+            for s in readable:
+                sendSize = 0
+                if pos + recvedPacket < len(self.packets):
+                    sendSize = len(self.packets[pos].packet)
+                r = s.recv(100000000)
 
-					compareSize = len(packet) - recved
-					if len(r) < compareSize:
-						compareSize = len(r)
-					recved += compareSize
-					if recved == len(packet):
-						recvedPacket += 1
-						if recvedPacket == packetLen:
-							if len(r) != compareSize:
-								# has extra packet.
-								sys.stdout.flush()
-								break
-						recved = 0
-						sys.stdout.flush()
+                while r:
+                    if recvedPacket >= packetLen:
+                        print('X', end=' ')
+                        sys.stdout.flush()
+                        break
+                    packet = self.packets[pos + recvedPacket].packet
 
-					r = r[compareSize:]
+                    compareSize = len(packet) - recved
+                    if len(r) < compareSize:
+                        compareSize = len(r)
+                    recved += compareSize
+                    if recved == len(packet):
+                        recvedPacket += 1
+                        if recvedPacket == packetLen:
+                            if len(r) != compareSize:
+                                # has extra packet.
+                                sys.stdout.flush()
+                                break
+                        recved = 0
+                        sys.stdout.flush()
 
-			for s in writeable:
-				print(pos)
-				packet = self.packets[pos].packet
-				if sended == 0:
-					r = s.send(packet)
+                    r = r[compareSize:]
 
-				else:
-					r = s.send(packet[sended:])
-				if r > 0:
-					sended += r
-					if len(packet) <= sended:
-						sended = 0
-						sendedPacket += 1
-						if self.sleep != 0:
-							time.sleep(self.sleep)
-		except Exception as e:
-			print("error")
+            for s in writeable:
+                print(pos)
+                packet = self.packets[pos].packet
+                if sended == 0:
+                    r = s.send(packet)
 
-def runTest(process_count = 128, thread_count = 100):
-	datafile = 'db_test.txt'
-	repeat = 1
-	time_out = 5
-	sleep_time = 0
-	verbose = False
+                else:
+                    r = s.send(packet[sended:])
+                if r > 0:
+                    sended += r
+                    if len(packet) <= sended:
+                        sended = 0
+                        sendedPacket += 1
+                        if int(self.conf['SLEEP']) != 0:
+                            time.sleep(int(self.conf['SLEEP']))
+        except Exception as e:
+            print("error")
 
-	if not os.path.exists(datafile):
-		print('ERROR:', datafile, 'is not exist.')
-		sys.exit(-1)
+class DbmsSever:
+    def __init__(self):
+        pass
+    def run():
+        conf = commonlib.readConfFile(CONFPATH)
 
-	hexdata = commonlib.readFileLines(datafile)
-	packets = packetutil.PacketReader.read(hexdata)
+        packet_list = conf['PACKET_LIST_CSV']
+        packet_list = commonlib.getlistfromcsv(packet_list)
 
-	totalTest = repeat * process_count
-	curCount = 0
+        target_list = conf['SERVER_LIST_CSV']
+        target_list = commonlib.getlistfromcsv(target_list)
 
-	def callback(arg):
-		arg[0] = arg[0] + 1
-		print('%d/%d' % (totalTest, arg[0]), end=' ')
+        cert_info_list = conf['CERT_LIST_CSV']
+        cert_info_list = commonlib.getlistfromcsv(cert_info_list)
 
-	callback_arg = [curCount]
+        # 프로세스 개수와 타겟 개수가 1대1 매칭이 안될 경우
+        if len(target_list) != int(conf['SERVICE_COUNT']):
+            return
 
-	start_time = time.time()
+        if len(packet_list) != int(conf['SERVICE_COUNT']):
+            return
 
-	process_list = []
+        process_count = int(conf['SERVICE_COUNT'])
 
-	q = Queue()
+        curCount = 0
 
-	for i in range(process_count):
-		connecte = packetutil.VirtualServer(3306 + i, "192.168.4.87", 3970,1)
-		process_list.append(Worker(i + 1, connecte, packets,
-							  time_out, repeat, sleep_time, verbose,
-							  callback, callback_arg,thread_count,q))
+        def callback(arg):
+            arg[0] = arg[0] + 1
 
-	for i in process_list:
-		print('process starting : ', i.num)
-		i.start()
-	for i in process_list:
-		i.join()
-		print('process joined.', i.num)
+        callback_arg = [curCount]
+        start_time = time.time()
 
-	q.close()
+        process_list = []
+        q = Queue()
 
-	elapsed_time = time.time() - start_time
-	print('\n')
-	print('Done. [%02d:%02d:%02.2d]' % (int(elapsed_time) / 60 / 60, int(elapsed_time) / 60 % 60, elapsed_time % 60))
+        for i in range(process_count):
+            hexdata = commonlib.readFileLines(str(''.join(packet_list[i])))
+            packets = packetutil.PacketReader.read(hexdata)
+            dbms_sock_object = packetutil.VirtualServer(service_port=int(target_list[i][2]), dbsafer_ip=conf['DBSAFER_GW_IP'])
+            process_list.append(Worker(i + 1, dbms_sock_object, packets,
+                                       conf, target_list[i], callback, callback_arg, q))
+
+        for i in process_list:
+            print('process starting : ', i.num)
+            i.start()
+        for i in process_list:
+            i.join()
+            print('process joined.', i.num)
+
+        q.close()
+
+        elapsed_time = time.time() - start_time
+        print('\n')
+        print('Done. [%02d:%02d:%02.2d]' % (int(elapsed_time) / 60 / 60, int(elapsed_time) / 60 % 60, elapsed_time % 60))
 
 if __name__ == '__main__':
-	#cProfile.run("runTest(1,2)")
-	runTest(30,50)
+    abc = DbmsSever()
+    abc.run()
